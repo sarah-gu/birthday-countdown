e4e09e820c37c03b3cf9e1d53e58502a417fd5ba
Sarah Gu <guwen888@gmail.com>
2024-02-03 16:16:22 -0500

    22nd bday

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import styles from "@/styles/Home.module.css";
 import "react-datetime/css/react-datetime.css";
 const inter = Inter({ subsets: ["latin"] });
 
-const IMAGE_WIDTH = 275;
+const IMAGE_WIDTH = 200;
 
 function pad2Digits(num: number) {
   return num.toString().padStart(2, "0");
@@ -15,7 +15,7 @@ function pad2Digits(num: number) {
 const calcTimeLeft = () => {
   const date = new Date();
   // const [time, setTime] = useState({hours, minutes, seconds});
-  const difference = +new Date(`09/21/2023`) - +date;
+  const difference = +new Date(`02/04/2024`) - +date;
   if (difference >= 0) {
     const days = Math.floor(difference / (1000 * 60 * 60 * 24));
     const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
@@ -37,17 +37,6 @@ const calcTimeLeft = () => {
   }
 };
 
-const FunImage = () => {
-  return (
-    <Image
-      src="/images/"
-      width={IMAGE_WIDTH}
-      height="150"
-      alt="google picture"
-    />
-  );
-};
-
 const generateImgLoc = () => {
   if (typeof window === "undefined") {
     return { top: 0, left: 0, src: "/images/1.JPG" };
@@ -57,7 +46,7 @@ const generateImgLoc = () => {
   );
 
   const left = Math.floor(Math.random() * (window?.innerWidth - IMAGE_WIDTH));
-  const imgNum = Math.floor(Math.random() * 4 + 1);
+  const imgNum = Math.floor(Math.random() * 26 + 1);
 
   var top = topPre;
   if (topPre > 200 && topPre < 400) {
@@ -98,6 +87,14 @@ export default function Home() {
     return () => clearInterval(updateImg2);
   }, [setImgLoc2]);
 
+  const [imgLoc3, setImgLoc3] = useState(generateImgLoc());
+  useEffect(() => {
+    const updateImg3 = setInterval(() => {
+      setImgLoc3(generateImgLoc());
+    }, 6000);
+    return () => clearInterval(updateImg3);
+  }, [setImgLoc2]);
+
   const [textLoc, setTextLoc] = useState(generateImgLoc());
   useEffect(() => {
     const updateText = setInterval(() => {
@@ -114,49 +111,78 @@ export default function Home() {
     return () => clearInterval(updateText);
   }, [setTextLoc2]);
 
+  const [textLoc3, setTextLoc3] = useState(generateImgLoc());
+  useEffect(() => {
+    const updateText = setInterval(() => {
+      setTextLoc3(generateImgLoc());
+    }, 4000);
+    return () => clearInterval(updateText);
+  }, [setTextLoc3]);
+
+  const [textLoc4, setTextLoc4] = useState(generateImgLoc());
+  useEffect(() => {
+    const updateText = setInterval(() => {
+      setTextLoc4(generateImgLoc());
+    }, 4000);
+    return () => clearInterval(updateText);
+  }, [setTextLoc4]);
+
   return (
-    <div className="bg-gradient-to-r from-purple-300 to-yellow-100 h-screen w-screen flex flex-col flex-grow justify-center relative font-sans">
+    <div className="bg-gradient-to-tr from-fuchsia-500 to-cyan-500 h-screen w-screen flex flex-col flex-grow justify-center relative font-sans">
       <div className="flex flex-col text-[100px] w-screen h-screen items-center justify-center">
-        <div className="z-[10000] bg-white p-4 m-4 rounded-lg shadow-lg px-[75px] w-[1000px]">
+        <div className="z-[10000] bg-white p-4 m-4 rounded-lg shadow-lg px-[75px] w-[800px]">
           <div className="text-[30px] text-slate-600 justify-center align-center h-[50px] w-[500px] grow-0">
-            MEGHAN&apos;S 21ST BIRTHDAY!!!! 😎
+            SARAH&apos;S 22ND BIRTHDAY!!!! 😎
           </div>
           <div className="pb-0">
-            {timeLeft.days}
-            <span className="text-[40px] p-1">D&nbsp;</span>:{timeLeft.hours}
+            {timeLeft.hours}
             <span className="text-[40px] p-1">H&nbsp;</span>:{timeLeft.minutes}
             <span className="text-[40px] p-1">M&nbsp;</span>:{timeLeft.seconds}
             <span className="text-[40px] p-1">S&nbsp;</span>
           </div>
         </div>
 
-        {/* <div
+        <div
           style={{ top: imgLoc.top, left: imgLoc.left }}
-          className={`fixed rounded-lg animate-fade-in  w-[300px] h-[300px]`}
+          className={`fixed rounded-full animate-fade-in  w-[200px] h-[200px]`}
           key={imgLoc.left}
         >
           <Image
             src={`${imgLoc.src}`}
-            className="rounded-lg animate-fade-in"
-            width="275"
-            height="150"
+            className="rounded-full animate-fade-in"
+            width="200"
+            height="200"
             alt="google picture"
           />
         </div>
 
         <div
           style={{ top: imgLoc2.top, left: imgLoc2.left }}
-          className={`fixed rounded-lg animate-fade-in w-[200px] h-[200px]`}
+          className={`fixed rounded-full animate-fade-in w-[200px] h-[200px]`}
           key={imgLoc2.left}
         >
           <Image
             src={`${imgLoc2.src}`}
-            className="rounded-lg overflow-clip animate-fade-in "
-            width="275"
-            height="150"
+            className="rounded-full h-50 w-50 overflow-clip animate-fade-in "
+            width="200"
+            height="200"
             alt="google picture"
           />
-        </div> */}
+        </div>
+
+        <div
+          style={{ top: imgLoc3.top, left: imgLoc3.left }}
+          className={`fixed rounded-full animate-fade-in w-[200px] h-[200px]`}
+          key={imgLoc3.left}
+        >
+          <Image
+            src={`${imgLoc3.src}`}
+            className="rounded-full h-50 w-50 overflow-clip animate-fade-in "
+            width="200"
+            height="200"
+            alt="google picture"
+          />
+        </div>
 
         <div
           style={{ top: textLoc.top, left: textLoc.left }}
@@ -172,6 +198,20 @@ export default function Home() {
           key={textLoc2.left}
         >
           🎉
+        </div>
+        <div
+          style={{ top: textLoc3.top, left: textLoc3.left }}
+          className={`fixed rounded-lg animate-fade-in w-[600px] h-[200px] text-[40px]`}
+          key={textLoc3.left}
+        >
+          sarah
+        </div>
+        <div
+          style={{ top: textLoc4.top, left: textLoc4.left }}
+          className={`fixed rounded-lg animate-fade-in w-[600px] h-[200px] text-[40px]`}
+          key={textLoc4.left}
+        >
+          bday
         </div>
       </div>
     </div>
